b11338c6a648dbf15093591db2e5bcc4349451b0
Fyke Lleva <floterina@gmail.com>
2024-10-04 12:50:35 +0800

    updated

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, session, redirect
 from database import Tables
 from accounts import *
 from category import *
@@ -6,14 +6,26 @@ from product import *
 from sales import *
 from stocks import *
 app = Flask(__name__)
+app.secret_key = 'smartretail'
 
 @app.route('/')
 def login():
-    return render_template('login.html')
+    if 'user_type' in session and session['user_type'] == 1:
+        redirect ['/admin_dashboard']
+    else: 
+        return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
 
 @app.route('/admin-dashboard')
 def admin_dashboard():
-    return render_template('admin-dashboard.html')
+    if 'user_type' in session and session['user_type'] == 1:
+        return render_template('admin-dashboard.html')
+    else:
+        return redirect('/')
 
 @app.route('/cashier-pos')
 def cashier_pos():
@@ -26,6 +38,7 @@ def post_login():
     uname = json.get('uname')
     passw = json.get('passw')
     if uname == 'admin' and passw == 'admin':
+        session['user_type'] = 1
         return '1'
     else:
         return '0'
